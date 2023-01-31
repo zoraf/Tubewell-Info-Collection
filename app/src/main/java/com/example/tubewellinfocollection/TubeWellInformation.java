6b21df9e6ea7ff88be23a3c0a8428e30fe3f89fd
zoraf <mzoraf09@gmail.com>
2023-01-31 15:57:43 +0600

    material design is added

--- app/src/main/java/com/example/tubewellinfocollection/TubeWellInformation.java
+++ app/src/main/java/com/example/tubewellinfocollection/TubeWellInformation.java
@@ -16,15 +16,15 @@ public class TubeWellInformation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tube_well_information);
-        init();
+//        init();
     }
 
-    private void init(){
-        etOwnerName = findViewById(R.id.etOwnerName);
-        spnOwnerType = findViewById(R.id.spnOwnerType);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.owner_type, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spnOwnerType.setAdapter(adapter);
-    }
+//    private void init(){
+//        etOwnerName = findViewById(R.id.etOwnerName);
+//        spnOwnerType = findViewById(R.id.spnOwnerType);
+//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+//                R.array.owner_type, android.R.layout.simple_spinner_item);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spnOwnerType.setAdapter(adapter);
+//    }
 }
