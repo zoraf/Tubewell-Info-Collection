827f1464b2c30ac326cff1240e5147e6a4032ac5
zoraf <mzoraf09@gmail.com>
2023-05-29 15:27:36 +0600

    server ip is added to api utils

--- app/src/main/java/com/example/tubewellinfocollection/Service/ApiUtils.java
+++ app/src/main/java/com/example/tubewellinfocollection/Service/ApiUtils.java
@@ -3,7 +3,7 @@ package com.example.tubewellinfocollection.Service;
 public class ApiUtils {
 
     private ApiUtils() {}
-    public static final String BASE_URL = "http://localhost:8080/api/";
+    public static final String BASE_URL = "http://192.168.100.44:8080/api/";
     //    public static final String BASE_URL = "http://192.168.0.105/bramptionhardwood-Inventory/bramptionhardwood-Inventory/services/";
     public static ApiService getAPIService() {
         return RetrofitClient.getClient(BASE_URL).create(ApiService.class);
